86faf12b22d3af55e1e1a8d876fade214375c7fa
gokkky <goku_dera_hayato@hotmail.com>
2019-10-05 00:44:21 +0700

    commit

--- script.js
+++ script.js
@@ -1,49 +1,13 @@
-let count = 3; //นับจำนวนครั้งที่ผิด
-let num = 1; //เลขข้อ
-var seconds = 10; //เวลาที่กดหนด
-var t;
-
-function timedCount() {
-    //document.getElementById("time").value = c;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    document.getElementById("time").innerHTML = "00:" + seconds;
-    document.getElementById("count").innerHTML = count;
-    seconds -= 1;
-    //สำหรับการrandomคำถามใหม่เมื่อหมดเวลา
-    if (count > 0 && seconds < 0) {
-        //clearTimeout(t);
-        count--;
-        if (count != 0) {
-            //ดักการrandomใหม่เมื่อ count=0
-            n = Math.floor(Math.random() * 10);
-            ++num;
-        }
-        seconds = 10;
-        document.getElementById("num").innerHTML = num;
-        document.getElementById("demo").innerHTML = lv1[n] + " = ";
-        //timedCount();
-    }
-    //เรียกตัวเอง
-    t = setTimeout(timedCount, 1000);
-    //ถ้าcountเป็น0จะแสดง lost time แล้วก็เคลียค่า t
-    if (count == 0) {
-        document.getElementById("lost").innerHTML = "LOSE TIME";
-        document.getElementById("count").innerHTML = count;
-        clearTimeout(t);
-    }
-
-}
-
-//คำถามและคำตอบ
-const lv1 = ['4 + 5', '9 + 6', '7 + 8', '10 + 13', '1 + 9', '7 + 3', '19 + 4', '16 + 18', '3 + 3', '0 + 9', '5 - 3', '6 - 4', '5 - 5', '3 - 1'],
-    ans1 = ['9', '15', '15', '23', '10', '10', '23', '34', '6', '9'];
-
+let count = 2, num = 0, seconds = 10;
+let t, sum, icon = false;
 let score = 0; //คะแนน
-let n = Math.floor(Math.random() * 10); //เริ่มต้นrandom
+
+timedCount();
+//randomโจทย์
+random();
+
 //แสดงค่า
-document.getElementById("num").innerHTML = num;
 document.getElementById("score").innerHTML = score;
-document.getElementById("demo").innerHTML = lv1[n] + " =&nbsp;";
 
 //Set speech recognition
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -58,20 +22,18 @@ recognition.onresult = (e) => {
             transcript = e.results[i][0].transcript;
         }
     }
-    //const transcript = e.results[0][0].transcript;
     //กรณีตอบถูก
-    if (ans1[n] == transcript) {
+    if (sum.toString() == transcript.trim()) {
         //alert("true");
-        ++score;
+        icon = true;
+        score += 10;
         document.getElementById("test").innerHTML = transcript;
-        document.getElementById("lost").innerHTML = "current";
-        transcript = '';
+        //transcript = '';
     }
-    /*if (transcript.includes('hi')) {
-        alert("Hi");
-        transcript = '';
-    }*/
-    else document.getElementById("lost").innerHTML = "fail";
+    else {
+        --count;
+
+    }
     //random
     random();
     //Output transcript
@@ -80,13 +42,111 @@ recognition.onresult = (e) => {
 
 
 }
-//เรียกให้เริ่มใหม่
-//recognition.addEventListener('end', recognition.start);
 
+//recognition.addEventListener('end', recognition.start);
+function timedCount() {
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    document.getElementById("time").innerHTML = "00:" + seconds;
+    document.getElementById("count").innerHTML = count;
+    seconds -= 1;
+    t = setTimeout(timedCount, 1000);
+
+    //สำหรับการrandomคำถามใหม่เมื่อหมดเวลา
+    if (count > -1 && seconds < 0) {
+        //clearTimeout(t);
+        count--;
+        //ดักการrandomใหม่เมื่อ count=-1
+        if (count != -1) {
+            random();
+        }
+        //seconds = 10;
+        //random();
+        //timedCount();
+    }
+    //เรียกตัวเอง
+
+    //ถ้าcountเป็น0จะแสดง lost time แล้วก็เคลียค่า t
+    if (count == -1) {
+        document.getElementById("count").innerHTML = 0;
+        swal({
+            text: "LOST TIME!",
+            icon: "error",
+            timer: 5000,
+            button: false
+        });
+        recognition.stop();
+        clearTimeout(t);
+    }
+
+}
+
+function noti() {
+    if (num != 0) {
+        if (icon) {
+            icon = false;
+            swal({
+                icon: "success",
+                button: false,
+                timer: 1500
+            });
+        }
+        else {
+
+            swal({
+                icon: "error",
+                button: false,
+                timer: 1500
+            });
+        }
+
+    }
+
+}
 
 function random() {
-    n = Math.floor(Math.random() * 10);
-    document.getElementById("demo").innerHTML = lv1[n] + " = ";
+    noti();
+
+    const operator = ['+', '-', '*', '/'];
+    let op = operator[Math.floor(Math.random() * 2)];
+    op = operator[1];
+    let n1 = Math.floor(Math.random() * 20);
+    let n2 = Math.floor(Math.random() * 20);
+    if (op == '+') {
+        document.getElementById("demo").innerHTML = n1 + " + " + n2 + " =&nbsp;";
+        sum = n1 + n2;
+        while (sum == 10) {
+            n1 = Math.floor(Math.random() * 20);
+            n2 = Math.floor(Math.random() * 20);
+        }
+    }
+    else if (op == '-') {
+        while (n2 > n1) {
+            n1 = Math.floor(Math.random() * 20);
+            n2 = Math.floor(Math.random() * 20);
+        }
+        document.getElementById("demo").innerHTML = n1 + " - " + n2 + " =&nbsp;";
+        sum = n1 - n2;
+        while (sum == 10) {
+            n1 = Math.floor(Math.random() * 20);
+            n2 = Math.floor(Math.random() * 20);
+        }
+    }
+    else if (op == '*') {
+        document.getElementById("demo").innerHTML = n1 + " * " + n2 + " =&nbsp;";
+        sum = n1 * n2;
+        while (sum == 10) {
+            n1 = Math.floor(Math.random() * 20);
+            n2 = Math.floor(Math.random() * 20);
+        }
+    }
+    else {
+        document.getElementById("demo").innerHTML = n1 + " / " + n2 + " =&nbsp;";
+        sum = n1 / n2;
+        while (sum == 10) {
+            n1 = Math.floor(Math.random() * 20);
+            n2 = Math.floor(Math.random() * 20);
+        }
+    }
     seconds = 10;
     ++num;
     document.getElementById("num").innerHTML = num;
